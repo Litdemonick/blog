cecdfe7ee6862f34a13b05002893edd0c2d59c17
Carlos Miranda <badleon2744@gmail.com>
2025-09-26 17:24:38 -0500

    Corrección de duplicacion.

--- context_processors.py
+++
@@ -1,9 +0,0 @@
-from blog.models import Notification
-
-def unread_notifications(request):
-    """Agrega el contador de notificaciones no leídas al contexto global"""
-    if request.user.is_authenticated:
-        return {
-            "unread_count": request.user.notifications.filter(is_read=False).count()
-        }
-    return {"unread_count": 0}
